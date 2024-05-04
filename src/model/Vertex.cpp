bac719130c5468a28d2fe48f6130d681dd8d45bf
elliothacker33 <tomastorrespereira@gmail.com>
2024-05-04 18:06:13 +0100

    add vertex class

---
+++ src/model/Vertex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tomas on 04-05-2024.
+//
+
+#include "Vertex.h"
---
+++ src/model/Vertex.h
@@ -0,0 +1,14 @@
+//
+// Created by tomas on 04-05-2024.
+//
+
+#ifndef TSP_ANALYSIS_VERTEX_H
+#define TSP_ANALYSIS_VERTEX_H
+
+
+class Vertex {
+
+};
+
+
+#endif //TSP_ANALYSIS_VERTEX_H
